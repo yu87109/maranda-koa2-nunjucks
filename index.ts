ae3b4289b2d19e90d692c0a10d8e25c855a57c35
Sunny <yu87109@qq.com>
2019-04-22 16:47:53 +0800

    project init

---
+++ index.ts
@@ -0,0 +1,22 @@
+import {Environment, FileSystemLoader, ConfigureOptions} from 'nunjucks';
+import Koa from 'koa';
+
+interface CtxRender{
+    render: (this:Koa<any,CtxRender>, ViewName:string, RenderData:{}) => void;
+}
+export function Koa2Nunjucks(ViewPath:string, app:Koa<any, CtxRender>, EnvOptions?: ConfigureOptions){
+    const FileLoader = new FileSystemLoader(ViewPath);
+    const Env = new Environment(FileLoader, Object.assign({
+        autoescape: false,
+        throwOnUndefined: false,
+        trimBlocks: true,
+        lstripBlocks:true,
+        watch: true,
+        noCache: true,
+    }, EnvOptions));
+    app.context.render = function(this:Koa<any,CtxRender>, ViewName:string, RenderData:{}){
+        this.response.body = Env.render(ViewName, RenderData);
+        this.response.type = 'text/html';
+    }
+    return app;
+}
